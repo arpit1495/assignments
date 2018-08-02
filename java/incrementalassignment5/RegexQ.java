1eff64ccfe15347ec14770f77335d85335dca029
Arpit Maheshwari <arpit.maheshwari@zemosolabs.com>
2018-08-02 14:39:18 +0530

    Updated

---
+++ java/incrementalassignment5/RegexQ.java
@@ -0,0 +1,18 @@
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class RegexQ {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a sentence.");
+        String txt = input.nextLine();
+        boolean first = Pattern.matches("^[A-Z].*", txt);
+        boolean last = Pattern.matches(".*\\.$", txt);
+        if(first && last){
+            System.out.println("Yes!!");
+        }
+        else{
+            System.out.println("Sorry");
+        }
+    }
+}
