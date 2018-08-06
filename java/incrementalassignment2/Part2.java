40f132b7604772b67966358e190c61169009409a
Arpit Maheshwari <arpit.maheshwari@zemosolabs.com>
2018-08-06 20:26:06 +0530

    Updated

---
+++ java/incrementalassignment2/Part2.java
@@ -0,0 +1,9 @@
+public class Part2 {
+    int field;
+    public Part2(){
+        this(0);
+    }
+    public Part2(int num){
+        field = num;
+    }
+}
---
+++ java/incrementalassignment2/Part3.java
@@ -0,0 +1,8 @@
+public class Part3 {
+    Part3(String str){
+        System.out.println(str);
+    }
+    public static void main(String args[]){
+        Part3[] arr;
+    }
+}
