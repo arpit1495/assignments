b103ac453ffef35fbf11fcc14acf6a45def044df
Arpit Maheshwari <arpit.maheshwari@zemosolabs.com>
2018-08-06 20:40:39 +0530

    Updated

--- java/incrementalassignment2/Part4.java
+++ java/incrementalassignment2/Part4.java
@@ -1,12 +1,8 @@
-import java.util.LinkedList;
-import java.util.Map;
-
 public class Part4 {
     public Part4(String str){
         System.out.println(str);
     }
     public static void main(String args[]){
         Part4[] arr = {new Part4("hello"), new Part4("world")};
-        Map
     }
 }
